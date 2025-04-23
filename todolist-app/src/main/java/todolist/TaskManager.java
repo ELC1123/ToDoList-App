7629ed8bd2d65117b6bbc546bf6cb437cddaecd2
ELC1123 <ethan_lester_l_chan@dlsu.edu.ph>
2025-04-23 16:30:24 +0800

    just added ArrayList of Tasks

--- todolist-app/src/main/java/todolist/TaskManager.java
+++ todolist-app/src/main/java/todolist/TaskManager.java
@@ -2,8 +2,13 @@ package todolist;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class TaskManager {
+
+    private ArrayList<Task> taskList = new ArrayList<>();
 
     @FXML
     private void switchToSecondary() throws IOException {
