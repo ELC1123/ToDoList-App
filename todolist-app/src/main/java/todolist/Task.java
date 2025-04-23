b082c5902b440f7a8b7104e2bd45ba378dcc1757
ELC1123 <ethan_lester_l_chan@dlsu.edu.ph>
2025-04-23 16:29:19 +0800

    implemented task class

--- todolist-app/src/main/java/todolist/Task.java
+++ todolist-app/src/main/java/todolist/Task.java
@@ -3,5 +3,65 @@ package todolist;
 import java.time.*;
 
 public class Task {
+    // Initialize variables
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private String priority;
+    private boolean completionStatus;
+
+    // Constructor
+    public Task(String title, String description, LocalDate dueDate, String priority, boolean completionStatus) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completionStatus = completionStatus;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
     
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public boolean getCompletionStatus() {
+        return completionStatus;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public void setCompletionStatus(boolean completionStatus) {
+        this.completionStatus = completionStatus;
+    }
+
+    @Override public String toString() {
+        return "[" + (completionStatus ? "X" : " ") + "]" + title + " (Due: " + dueDate + ", Priority: " + priority + ")";
+    }
 }
