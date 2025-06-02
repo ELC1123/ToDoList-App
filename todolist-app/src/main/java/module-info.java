1a2547bb15507b0894e7699301c33d6804b8a2d2
ELC1123 <ethan_lester_l_chan@dlsu.edu.ph>
2025-06-03 00:02:42 +0800

    updated to add gson support

--- todolist-app/src/main/java/module-info.java
+++ todolist-app/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module todolist {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires com.google.gson;
 
-    opens todolist to javafx.fxml;
+    opens todolist to javafx.fxml, com.google.gson;
     exports todolist;
 }
